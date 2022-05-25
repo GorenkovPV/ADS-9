e5664a78248e9fc2c493ec203820e9bf986f1a36
GorenkovPV <94713562+GorenkovPV@users.noreply.github.com>
2022-05-25 17:21:55 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,30 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_BST_H_
-#define INCLUDE_BST_H_
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  "bst.h"
 
-#endif  // INCLUDE_BST_H_
+BST<std::string> makeTree(const char* filename) {
+  BST<std::string> tree;
+  std::string word = "";
+  char lower = 'a' - 'A';
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "Error!" << std::endl;
+    return tree;
+  }
+  while (!file.eof()) {
+    char symbol = file.get();
+    if ((symbol >= 'a' && symbol <= 'z') || (symbol >= 'A' && symbol <= 'Z')) {
+      if (symbol >= 'A' && symbol <= 'Z')
+        symbol += lower;
+      word += symbol;
+    } else if (word != "") {
+      tree.add(word);
+      word = "";
+    }
+  }
+  file.close();
+  return tree;
+}
