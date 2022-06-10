50cbf5dea399a298a6ee49dd885c451160a8afa2
GorenkovPV <94713562+GorenkovPV@users.noreply.github.com>
2022-06-10 23:15:38 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,30 +1,66 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#ifndef INCLUDE_BST_H_
+#define INCLUDE_BST_H_
 
-BST<std::string> makeTree(const char* filename) {
-  BST<std::string> tree;
-  std::string word = "";
-  char lower = 'a' - 'A';
-  std::ifstream file(filename);
-  if (!file) {
-    std::cout << "Error!" << std::endl;
-    return tree;
-  }
-  while (!file.eof()) {
-    char symbol = file.get();
-    if ((symbol >= 'a' && symbol <= 'z') || (symbol >= 'A' && symbol <= 'Z')) {
-      if (symbol >= 'A' && symbol <= 'Z')
-        symbol += lower;
-      word += symbol;
-    } else if (word != "") {
-      tree.add(word);
-      word = "";
+template<typename T>
+
+class BST {
+ private:
+  struct Node {
+    T val;
+    Node *rght;
+    Node *lft;
+    int count;
+  };
+  Node *rt;
+  Node *addNode(Node *rt, T x) {
+      if (rt == nullptr) {
+          rt = new Node;
+          rt->val = x;
+          rt->lft = nullptr;
+          rt->rght = nullptr;
+      } else {
+          if (rt->val > x) rt->lft = addNode(rt->lft, x);
+          if (rt->val < x) rt->rght = addNode(rt->rght, x);
+          if (rt->val == x) (rt->count)++;
+      }
+      return rt;
+    }
+
+  int searchValue(Node* rt, T x) {
+    if (rt == nullptr) {
+      return 0;
+    } else if (rt->val == x) {
+        return rt->count;
+    } else if (rt->val < x) {
+        return searchValue(rt->rght, x);
+    } else {
+        return searchValue(rt->lft, x);
     }
   }
-  file.close();
-  return tree;
-}
+
+  int dpth(Node* rt) {
+      if (rt == nullptr)
+        return 0;
+      if (rt->lft == nullptr && rt->rght == nullptr) return 0;
+      int rt = dpth(rt->rght), lt = dpth(rt->lft);
+      if (rt > lt)
+        return rt + 1;
+      else
+        return lt + 1;
+  }
+
+ public:
+  BST():rt(nullptr) {}
+  void add(T x) {
+      rt = addNode(rt, x);
+  }
+  int depth() {
+      return dpth(rt);
+  }
+  int search(T x) {
+      return searchValue(rt, x) + 1;
+  }
+};
+
+#endif  // INCLUDE_BST_H_
